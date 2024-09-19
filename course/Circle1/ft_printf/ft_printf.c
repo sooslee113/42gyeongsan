22b8b692872f22331c72679df128338e9930a4b6
sooslee113 <soosung113@gmail.com>
2024-09-19 20:50:41 +0900

    remove double pointer

--- course/Circle1/ft_printf/ft_printf.c
+++ course/Circle1/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: sooslee <sooslee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 14:50:18 by sooslee           #+#    #+#             */
-/*   Updated: 2024/06/15 15:41:29 by sooslee          ###   ########.fr       */
+/*   Updated: 2024/09/19 20:49:32 by sooslee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,25 @@
 //#include <stdio.h>
 //#include "libft/libft.h"
 
-int	check_var_type(const char *str, va_list **ap)
+int	check_var_type(const char *str, va_list *ap)
 {
 	int	count;
 
 	count = 0;
 	if (*(str + 1) == 'c')
-		count += ft_putchar(va_arg(**ap, int));
+		count += ft_putchar(va_arg(*ap, int));
 	else if (*(str + 1) == 's')
-		count += ft_putstr(va_arg(**ap, char *));
+		count += ft_putstr(va_arg(*ap, char *));
 	else if (*(str + 1) == 'p')
-		count += ft_putptr(va_arg(**ap, void *));
+		count += ft_putptr(va_arg(*ap, void *));
 	else if (*(str + 1) == 'd' || *(str + 1) == 'i')
-		count += ft_putnbr(va_arg(**ap, int));
+		count += ft_putnbr(va_arg(*ap, int));
 	else if (*(str + 1) == 'u')
-		count += ft_putunsignn(va_arg(**ap, unsigned int));
+		count += ft_putunsignn(va_arg(*ap, unsigned int));
 	else if (*(str + 1) == 'x')
-		count += ft_puthex(va_arg(**ap, unsigned int));
+		count += ft_puthex(va_arg(*ap, unsigned int));
 	else if (*(str + 1) == 'X')
-		count += ft_puthex2(va_arg(**ap, unsigned int));
+		count += ft_puthex2(va_arg(*ap, unsigned int));
 	else if (*(str + 1) == '%')
 		count += write(1, "%", 1);
 	return (count);
@@ -49,7 +49,7 @@ int	vaarg(const char *str, va_list *ap)
 	{
 		if (*str == '%')
 		{
-			count += check_var_type(str, &ap);
+			count += check_var_type(str, ap);
 			str ++;
 		}
 		else
@@ -70,7 +70,7 @@ int	ft_printf(const char *format, ...)
 	va_end(ap);
 }
 
-// int main()
-// {
-// 	ft_printf("%s", "hello world");
-// }
+int main()
+{
+	ft_printf("%s", "hello world");
+}
